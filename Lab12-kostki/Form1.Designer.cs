f2c48611dd86a38ab5c5bc1669014e3dd096b1d0
Adam-Rem <80329664+Adam-Rem@users.noreply.github.com>
2023-01-21 20:44:29 +0100

    Add files via upload

---
+++ Lab12-kostki/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace Lab12_kostki
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.D2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.D4 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.D6 = new System.Windows.Forms.Label();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.D8 = new System.Windows.Forms.Label();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.D10 = new System.Windows.Forms.Label();
+            this.numericUpDown6 = new System.Windows.Forms.NumericUpDown();
+            this.D12 = new System.Windows.Forms.Label();
+            this.numericUpDown7 = new System.Windows.Forms.NumericUpDown();
+            this.D20 = new System.Windows.Forms.Label();
+            this.numericUpDown8 = new System.Windows.Forms.NumericUpDown();
+            this.D100 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // D2
+            // 
+            this.D2.BackColor = System.Drawing.Color.Red;
+            this.D2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.D2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.D2.Location = new System.Drawing.Point(17, 9);
+            this.D2.Name = "D2";
+            this.D2.Size = new System.Drawing.Size(120, 100);
+            this.D2.TabIndex = 0;
+            this.D2.Text = "D2";
+            this.D2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(17, 112);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(143, 112);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 3;
+            // 
+            // D4
+            // 
+            this.D4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.D4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.D4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.D4.Location = new System.Drawing.Point(143, 9);
+            this.D4.Name = "D4";
+            this.D4.Size = new System.Drawing.Size(120, 100);
+            this.D4.TabIndex = 2;
+            this.D4.Text = "D4";
+            this.D4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(269, 112);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 5;
+            // 
+            // D6
+            // 
+            this.D6.BackColor = System.Drawing.Color.Yellow;
+            this.D6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.D6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.D6.Location = new System.Drawing.Point(269, 9);
+            this.D6.Name = "D6";
+            this.D6.Size = new System.Drawing.Size(120, 100);
+            this.D6.TabIndex = 4;
+            this.D6.Text = "D6";
+            this.D6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(395, 112);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown4.TabIndex = 7;
+            // 
+            // D8
+            // 
+            this.D8.BackColor = System.Drawing.Color.Lime;
+            this.D8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.D8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.D8.Location = new System.Drawing.Point(395, 9);
+            this.D8.Name = "D8";
+            this.D8.Size = new System.Drawing.Size(120, 100);
+            this.D8.TabIndex = 6;
+            this.D8.Text = "D8";
+            this.D8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.Location = new System.Drawing.Point(521, 112);
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown5.TabIndex = 9;
+            // 
+            // D10
+            // 
+            this.D10.BackColor = System.Drawing.Color.Cyan;
+            this.D10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.D10.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.D10.Location = new System.Drawing.Point(521, 9);
+            this.D10.Name = "D10";
+            this.D10.Size = new System.Drawing.Size(120, 100);
+            this.D10.TabIndex = 8;
+            this.D10.Text = "D10";
+            this.D10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDown6
+            // 
+            this.numericUpDown6.Location = new System.Drawing.Point(647, 112);
+            this.numericUpDown6.Name = "numericUpDown6";
+            this.numericUpDown6.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown6.TabIndex = 11;
+            // 
+            // D12
+            // 
+            this.D12.BackColor = System.Drawing.Color.Blue;
+            this.D12.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.D12.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.D12.Location = new System.Drawing.Point(647, 9);
+            this.D12.Name = "D12";
+            this.D12.Size = new System.Drawing.Size(120, 100);
+            this.D12.TabIndex = 10;
+            this.D12.Text = "D12";
+            this.D12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDown7
+            // 
+            this.numericUpDown7.Location = new System.Drawing.Point(773, 112);
+            this.numericUpDown7.Name = "numericUpDown7";
+            this.numericUpDown7.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown7.TabIndex = 13;
+            // 
+            // D20
+            // 
+            this.D20.BackColor = System.Drawing.Color.Fuchsia;
+            this.D20.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.D20.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.D20.Location = new System.Drawing.Point(773, 9);
+            this.D20.Name = "D20";
+            this.D20.Size = new System.Drawing.Size(120, 100);
+            this.D20.TabIndex = 12;
+            this.D20.Text = "D20";
+            this.D20.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDown8
+            // 
+            this.numericUpDown8.Location = new System.Drawing.Point(899, 112);
+            this.numericUpDown8.Name = "numericUpDown8";
+            this.numericUpDown8.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown8.TabIndex = 15;
+            // 
+            // D100
+            // 
+            this.D100.BackColor = System.Drawing.Color.Gray;
+            this.D100.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.D100.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.D100.Location = new System.Drawing.Point(899, 9);
+            this.D100.Name = "D100";
+            this.D100.Size = new System.Drawing.Size(120, 100);
+            this.D100.TabIndex = 14;
+            this.D100.Text = "D100";
+            this.D100.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.button1.Location = new System.Drawing.Point(1025, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 123);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Rzuć!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(22, 214);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1066, 186);
+            this.tableLayoutPanel1.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1131, 426);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown8);
+            this.Controls.Add(this.D100);
+            this.Controls.Add(this.numericUpDown7);
+            this.Controls.Add(this.D20);
+            this.Controls.Add(this.numericUpDown6);
+            this.Controls.Add(this.D12);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.D10);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.D8);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.D6);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.D4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.D2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown8)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label D2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label D4;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label D6;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Label D8;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.Label D10;
+        private System.Windows.Forms.NumericUpDown numericUpDown6;
+        private System.Windows.Forms.Label D12;
+        private System.Windows.Forms.NumericUpDown numericUpDown7;
+        private System.Windows.Forms.Label D20;
+        private System.Windows.Forms.NumericUpDown numericUpDown8;
+        private System.Windows.Forms.Label D100;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ Lab12-kostki/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab12_kostki
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            numericUpDown1.Maximum = 10;
+            numericUpDown2.Maximum = 10;
+            numericUpDown3.Maximum = 10;
+            numericUpDown4.Maximum = 10;
+            numericUpDown5.Maximum = 10;
+            numericUpDown6.Maximum = 10;
+            numericUpDown7.Maximum = 10;
+            numericUpDown8.Maximum = 10;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            tableLayoutPanel1.Controls.Clear();        
+            tableLayoutPanel1.ColumnCount= 10;
+            int ccount = 0;
+            tableLayoutPanel1.RowCount= 8;      
+            
+                     
+
+            for (int i = 0; i < (int)numericUpDown1.Value; i++)
+            {
+                Label newLabel = new Label();
+                newLabel.BackColor = Color.Red;
+                newLabel.ForeColor = Color.White;
+                newLabel.TextAlign = ContentAlignment.MiddleCenter;
+                Random n = new Random();
+                newLabel.Text = n.Next(1, 3).ToString();
+                tableLayoutPanel1.Controls.Add(newLabel,ccount,0);
+                ccount++;
+                System.Threading.Thread.Sleep(2);
+            }
+            ccount= 0;
+            for (int i = 0; i < (int)numericUpDown2.Value; i++)
+            {
+                Label newLabel = new Label();
+                newLabel.BackColor = Color.FromArgb(255, 128, 0);
+                newLabel.ForeColor = Color.White;
+                newLabel.TextAlign = ContentAlignment.MiddleCenter;
+                Random n = new Random();
+                newLabel.Text = n.Next(1, 5).ToString();
+                tableLayoutPanel1.Controls.Add(newLabel, ccount, 1);
+                ccount++;                
+                System.Threading.Thread.Sleep(2);
+            }
+            ccount= 0;
+            for (int i = 0; i < (int)numericUpDown3.Value; i++)
+            {
+                Label newLabel = new Label();
+                newLabel.BackColor = Color.Yellow;
+                newLabel.ForeColor = Color.Black;
+                newLabel.TextAlign = ContentAlignment.MiddleCenter;
+                Random n = new Random();
+                newLabel.Text = n.Next(1, 7).ToString();
+                tableLayoutPanel1.Controls.Add(newLabel, ccount, 2);
+                ccount++; ;
+                System.Threading.Thread.Sleep(2);
+            }
+            ccount= 0;
+            for (int i = 0; i < (int)numericUpDown4.Value; i++)
+            {
+                Label newLabel = new Label();
+                newLabel.BackColor = Color.Lime;
+                newLabel.ForeColor = Color.Black;
+                newLabel.TextAlign = ContentAlignment.MiddleCenter;
+                Random n = new Random();
+                newLabel.Text = n.Next(1, 9).ToString();
+                tableLayoutPanel1.Controls.Add(newLabel, ccount, 3);
+                ccount++; ;
+                System.Threading.Thread.Sleep(2);
+            }
+            ccount = 0;
+            for (int i = 0; i < (int)numericUpDown5.Value; i++)
+            {
+                Label newLabel = new Label();
+                newLabel.BackColor = Color.Cyan;
+                newLabel.ForeColor = Color.Black;
+                newLabel.TextAlign = ContentAlignment.MiddleCenter;
+                Random n = new Random();
+                newLabel.Text = n.Next(1, 11).ToString();
+                tableLayoutPanel1.Controls.Add(newLabel, ccount, 4);
+                ccount++; ;
+                System.Threading.Thread.Sleep(2);
+            }
+            ccount = 0;
+            for (int i = 0; i < (int)numericUpDown6.Value; i++)
+            {
+                Label newLabel = new Label();
+                newLabel.BackColor = Color.Blue;
+                newLabel.ForeColor = Color.White;
+                newLabel.TextAlign = ContentAlignment.MiddleCenter;
+                Random n = new Random();
+                newLabel.Text = n.Next(1, 13).ToString();
+                tableLayoutPanel1.Controls.Add(newLabel, ccount, 5);
+                ccount++; ;
+                System.Threading.Thread.Sleep(2);
+            }
+            ccount = 0;
+            for (int i = 0; i < (int)numericUpDown7.Value; i++)
+            {
+                Label newLabel = new Label();
+                newLabel.BackColor = Color.Fuchsia;
+                newLabel.ForeColor = Color.White;
+                newLabel.TextAlign = ContentAlignment.MiddleCenter;
+                Random n = new Random();
+                newLabel.Text = n.Next(1, 21).ToString();
+                tableLayoutPanel1.Controls.Add(newLabel, ccount, 6);
+                ccount++; ;
+                System.Threading.Thread.Sleep(2);
+            }
+            ccount = 0;
+            for (int i = 0; i < (int)numericUpDown8.Value; i++)
+            {
+                Label newLabel = new Label();
+                newLabel.BackColor = Color.Gray;
+                newLabel.ForeColor = Color.White;
+                newLabel.TextAlign = ContentAlignment.MiddleCenter;
+                Random n = new Random();
+                newLabel.Text = n.Next(1, 101).ToString();
+                tableLayoutPanel1.Controls.Add(newLabel, ccount, 7);
+                ccount++; ;
+                System.Threading.Thread.Sleep(2);
+            }
+            ccount = 0;
+
+
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {            
+            
+        }
+    }
+}
